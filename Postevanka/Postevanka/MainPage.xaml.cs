25327d0ced2b132b343c15f7efad2568c24cbd76
zantramte <zan.tramte@gmail.com>
2021-03-29 19:02:41 +0200

    Add project files.

---
+++ Postevanka/Postevanka/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.SimpleAudioPlayer;
+using System.IO;
+using System.Reflection;
+
+namespace Postevanka
+{
+    public partial class MainPage : ContentPage
+    {
+        ISimpleAudioPlayer Igralnik;
+        Button Glavni_Gumb;
+       
+        public void Uredi()
+        {
+            Posast.Vrni_Prvo();
+            Posast.Vrni_Drugo();
+            Posast.Polni();
+            G.Text = Convert.ToString(Posast.Prvo + "  •  " + Posast.Drugo);
+
+            for (int indeks = 0; indeks < Panel.Children.Count; indeks++)
+            {
+                if (Panel.Children[indeks] is Button)
+                {
+                    Posast.Cifra = Posast.Nakljucno.Next(Posast.Stevilke.Count);
+                    Glavni_Gumb = (Button)Panel.Children[indeks];
+                    Glavni_Gumb.Text = Convert.ToString(Posast.Stevilke[Posast.Cifra]);
+                    Posast.Stevilke.RemoveAt(Posast.Cifra);
+                }
+            }
+        }
+
+        Stream GetStreamFromFile(string filename)
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream("Postevanka." + filename);
+            return stream;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Uredi();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button Gumb = sender as Button;
+
+            Posast.Moja_Izbira = Convert.ToInt32(Gumb.Text);
+
+            if (Posast.Preveri_ustreznost())
+            {
+                var stream = GetStreamFromFile("ja.mp3");
+                Igralnik = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+                Igralnik.Load(stream);
+                Igralnik.Play();
+                DisplayAlert("Pošastko sporoča", "BRAVO! Pravilna rešitev!", "Nadaljuj");
+                Uredi();
+            }
+
+            else
+            {
+                var stream = GetStreamFromFile("ne.mp3");
+                Igralnik = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+                Igralnik.Load(stream);
+                Igralnik.Play();
+                DisplayAlert("Pošastko sporoča", "NAROBE! Rešitev ni pravilna!", "Nadaljuj");
+                Uredi();
+            }
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var stream = GetStreamFromFile("cartoon.mp3");
+            Igralnik = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            Igralnik.Load(stream);
+            Igralnik.Play();
+            Uredi();
+        }
+    }
+}
---
+++ Postevanka/Postevanka/Posast.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Postevanka
+{
+    public class Posast
+    {
+        public static int Moja_Izbira, Zmnozek, Prvo, Drugo, Cifra;
+        public static Random Nakljucno = new Random();
+        private static List<int> Stevilke_kopija = new List<int>();
+        public static List<int> Stevilke = new List<int>();
+
+        public static bool Preveri_ustreznost()
+        {
+            if (Moja_Izbira == Zmnozek)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public static int Vrni_Prvo()
+        {
+            Prvo = Nakljucno.Next(0, 11);
+            return Prvo;
+        }
+
+        public static int Vrni_Drugo()
+        {
+            Drugo = Nakljucno.Next(0, 11);
+            return Drugo;
+        }
+
+        public static void Polni()
+        {
+            Zmnozek = Prvo * Drugo;
+            Stevilke_kopija.Clear();
+            Stevilke.Clear();
+            Stevilke.Add(Zmnozek);
+
+            for (int indeks = 0; indeks < 101; indeks++)
+            {
+                Stevilke_kopija.Add(indeks);
+
+                if (indeks == Zmnozek)
+                {
+                    Stevilke_kopija.RemoveAt(indeks);
+                }
+            }
+            
+            for (int indeks = 0; indeks < 9; indeks++)
+            {               
+                Cifra = Nakljucno.Next(Stevilke_kopija.Count);
+                Stevilke.Add(Cifra);
+                Stevilke_kopija.RemoveAt(Cifra);
+            }
+        }
+    }
+}
